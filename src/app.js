abde4a975bacb35ed6d2c3d0695892c89bd246a0
Stephania31 <acunaantunez@gmail.com>
2023-03-05 18:40:41 +0000

    todos los cambios subidos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let domain = [".com", ".net", ".us", ".uy"];
+
+const final = () => {
+  let resultado = pronouns[Math.floor(Math.random() * pronouns.length)];
+  resultado += adjectives[Math.floor(Math.random() * adjectives.length)];
+  resultado += nouns[Math.floor(Math.random() * nouns.length)];
+  resultado += domain[Math.floor(Math.random() * domain.length)];
+  return resultado;
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let finalDomain = document.querySelector("#finalDomain");
+  finalDomain.innerHTML = final();
 };
